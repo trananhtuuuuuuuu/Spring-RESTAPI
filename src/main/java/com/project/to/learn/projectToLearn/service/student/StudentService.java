8eaac69aa701cc66f2a87c7d437e5ba0f59ec537
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-04 15:31:49 +0700

    created interface of StudentService

---
+++ src/main/java/com/project/to/learn/projectToLearn/service/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.project.to.learn.projectToLearn.service.student;
+
+import java.util.List;
+
+import com.project.to.learn.projectToLearn.domain.Student;
+
+
+public interface StudentService {
+
+  List<Student> getAllStudent();
+
+  Student saveStudent(Student student);
+
+
+  Student updateStudent(long id, Student student);
+
+
+  Student getStudentById(long id);
+
+  void deleteStudentById(long id);
+  
+  
+}
