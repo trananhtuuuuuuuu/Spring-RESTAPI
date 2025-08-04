c982d8482a89a1f638b0f4259156db56e6fcab86
Tran Anh Tu <trananhtu01012004@gmail.com>
2025-08-04 15:33:37 +0700

    Id Invalid exception

---
+++ src/main/java/com/project/to/learn/projectToLearn/exception/IdInvalidException.java
@@ -0,0 +1,7 @@
+package com.project.to.learn.projectToLearn.exception;
+
+public class IdInvalidException extends Exception {
+  public IdInvalidException(String message){
+    super(message);
+  }
+}
